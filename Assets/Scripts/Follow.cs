a9b0281b7080cb5f3c2b3b91be31f95df25e87e6
Yolanda Collins <landa.collins14@gmail.com>
2019-03-03 17:51:42 +0000

    Adding shooting Enemy files

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -6,13 +6,19 @@ public class Follow : MonoBehaviour {
 
     public float speed;
     public float stoppingDistance;
+    public float retreatingDistance;
+    public float timeBtwnShots;
+    public float StartingTimeBtwnShots;
+
+    public GameObject projectile;
 
     private Transform target;
 
 	// Use this for initialization
 	void Start () {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwnShots = StartingTimeBtwnShots;
+
 	}
 	
 	// Update is called once per frame
@@ -21,6 +27,21 @@ public class Follow : MonoBehaviour {
         if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }else if (Vector2.Distance(transform.position, target.position) < stoppingDistance && Vector2.Distance(transform.position, target.position) > retreatingDistance)
+        {
+            transform.position = this.transform.position;
+        }else if (Vector2.Distance(transform.position, target.position) < retreatingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
+        }
+
+        if(timeBtwnShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwnShots = StartingTimeBtwnShots;
+        }else
+        {
+            timeBtwnShots -= Time.deltaTime;
         }
     }
 }
